0a99b61346da7726e9eba9923a3eeeeb59a2d035
Taaj Andrews <taajandrews@gmail.com>
2023-06-16 14:14:04 -0400

    Create Detination Model

--- journeymate/main_app/models.py
+++ journeymate/main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+TRANSPORTS = (
+  ('A', 'Airplane'),
+  ('T', 'Train'),
+  ('D', 'Drive'),
+  ('B', 'Bus'),
+)
+
+class Destination(models.Model):
+  start_date = models.DateField('Start Date')
+  end_date = models.DateField('End Date')
+  country = models.CharField(max_length=100)
+  state = models.CharField(max_length=100)
+  city = models.CharField(max_length=100)
+  transportation = models.CharField(
+    max_length=1,
+    choices=TRANSPORTS,
+    default=TRANSPORTS[0][0],
+  )
