30afd7eb81a209d95d0e01206e039a45043bfa2b
Patton <pattongrooms@rocketmail.com>
2023-06-16 20:34:18 -0400

    itinerary model edit

--- journeymate/main_app/views.py
+++ journeymate/main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Destination
+from .forms import ItineraryForm
 
 # Create your views here.
 
@@ -20,7 +21,12 @@ def destinations_index(request):
 
 def destinations_detail(request, destination_id):
     destination = Destination.objects.get(id=destination_id)
-    return render(request, "destinations/detail.html", {"destination": destination})
+    itinerary_form = ItineraryForm()
+    return render(
+        request,
+        "destinations/detail.html",
+        {"destination": destination, "itinerary_form": itinerary_form},
+    )
 
 
 class DestinationCreate(CreateView):
